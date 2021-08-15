7ffa45be00db61000bc9a204b8d34b07630e1e64
Rajwan Ahmed <rajwan@egeneration.co>
2021-08-15 16:23:26 +0600

    Integrate firebase into application & signin page

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,50 @@
+import firebase from "firebase/app";
+
+import "firebase/auth";
+import "firebase/firestore";
+
+const config = {
+  apiKey: "AIzaSyDHM5Iv673UwH5TcVYWcmDcnfhOtiveSwU",
+  authDomain: "crwn-react-app.firebaseapp.com",
+  projectId: "crwn-react-app",
+  storageBucket: "crwn-react-app.appspot.com",
+  messagingSenderId: "34841257567",
+  appId: "1:34841257567:web:314d64f8526acdd9fb1dea",
+};
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (error) {
+      console.error("error creating user", error.message);
+    }
+  }
+
+  return userRef;
+};
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({ prompt: "select_account" });
+
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
